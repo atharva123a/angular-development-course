6f19c98802c1aad3b23788ce8288102f842cab2e
= <atharvarsalokhe2@gmail.com>
2022-05-09 19:31:27 +0530

    directives

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -12,20 +12,26 @@ export class ServersComponent implements OnInit {
   allowAddServer = false;
   serverCreationStatus = 'Server not created!'
   serverName = 'Test';
-  constructor() { 
-    setTimeout(() => {
-      this.allowAddServer = true;
-    }, 2000)
-  }
+  serverCreated = false;
+  servers = ["TestServer 1", "TestServer 2"];
+
+  constructor() { }
 
   ngOnInit(): void {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created successfully! The name of the new server is ' + this.serverName;
   }
 
   onUpdateServer (event : Event){
     this.serverName = (<HTMLInputElement>event.target).value;
   }
+
+  isServerNameEmpty() {
+    return this.serverName.length == 0;
+  }
+
 }
