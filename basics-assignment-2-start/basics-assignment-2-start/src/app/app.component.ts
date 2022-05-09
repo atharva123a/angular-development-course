3623f11f0a1acd7705d2b533296c1a96a8531270
= <atharvarsalokhe2@gmail.com>
2022-05-09 12:01:03 +0530

    done with the assignment

--- basics-assignment-2-start/basics-assignment-2-start/src/app/app.component.ts
+++ basics-assignment-2-start/basics-assignment-2-start/src/app/app.component.ts
@@ -6,4 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  username = '';
+
+  isUsernameEmpty(){
+    return this.username.length == 0;
+  }
+
+  resetUsername(){
+    this.username = '';
+  }
 }
